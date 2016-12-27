ae6c372df0a43a4ae10c23d2e474153ff02914c6
MarkVerschuuren <Verschuuren.mark@gmail.com>
2016-12-27 21:07:16 +0100

    Quicksort
    Script which sorts a list containing integers using the quicksort
    method.

---
+++ src/MainClass.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Mark Verschuuren on 27-12-2016.
+ */
+public class MainClass {
+    public static void main(String[] args) {
+        int[] test = { 1,9,7,5,3,8,7,4};
+        QuickSort QuickSortObJ = new QuickSort();
+        QuickSortObJ.sort(test);
+    }
+}
